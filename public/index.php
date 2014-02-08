a36fe0d0a6452ea2dd1f2a67ef3346fa9bc4c085
armanc <>
2014-02-08 13:22:16 +0200

    pievienots composer+ slim

--- public/index.php
+++ public/index.php
@@ -1,2 +1,14 @@
 <?php
-phpinfo();
+require '../vendor/autoload.php';
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+
+$app->get('/', function () {
+    echo "This is index page! IT WORKZ!";
+});
+
+$app->run();
