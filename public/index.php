347847ef5c18f4a4e58c2ba49d4c3241d7428ced
Administrator <Administrator@A-PC>
2014-02-08 14:13:26 +0200

    pievienots composer+ slim+twig

--- public/index.php
+++ public/index.php
@@ -1,14 +1,22 @@
 <?php
 require '../vendor/autoload.php';
-\Slim\Slim::registerAutoloader();
 
-$app = new \Slim\Slim();
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
-});
+$app = new \Slim\Slim(array(
+    'debug' => true,
+    'view' => new \Slim\Views\Twig(),
+    'templates.path' => '../templates',
+));
 
-$app->get('/', function () {
-    echo "This is index page! IT WORKZ!";
-});
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true,
+);
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+);
+
+$app->get('/', function () use ($app) {
+    $app->render('index.html.twig');
+})->name('home');
 
 $app->run();
