a55a62e9f0b6a5ee789abafd4ec29a507512b79f
armanc <>
2014-02-26 01:20:29 +0200

    daivojam dziljaak less, bootstrap, twig. veidoju sadaljas.

--- public/index.php
+++ public/index.php
@@ -15,18 +15,37 @@ $view->parserExtensions = array(
     new \Slim\Views\TwigExtension(),
 );
 
+//$app->get('/', function () use ($app) {
+//    $app->render('index.html.twig');
+//})->name('home');
+
 $app->get('/', function () use ($app) {
-    $app->render('index.html.twig');
+    $app->render('index.html.twig', array("active" => "home"));
 })->name('home');
 
-$app->get('/about/', function () use ($app) {
-    $app->render('about.html.twig');
-})->name('about');
+$app->get('/company', function () use ($app) {
+    $app->render('pages/company.html.twig', array("active" => "company"));
+})->name('company');
 
-$app->get('/hello/:name', function ($name) use ($app) {
-    $app->render('hello.html.twig', array(
-            'name' => $name
-        ));
-})->name('hello');
+$app->get('/products', function () use ($app) {
+    $app->render('pages/products.html.twig', array("active" => "products"));
+})->name('products');
+
+$app->get('/product', function () use ($app) {
+    $app->render('pages/product.html.twig', array("active" => "product"));
+})->name('product');
+
+$app->get('/contacts', function () use ($app) {
+    $app->render('pages/contacts.html.twig', array("active" => "contacts"));
+})->name('contacts');
+
+// lai twig varetu izmantot kaut kadus mainigos, mainigie ir japadod (ar array)
+
+
+//$app->get('/hello/:name', function ($name) use ($app) {
+//    $app->render('hello.html.twig', array(
+//            'name' => $name
+//        ));
+//})->name('hello');
 
 $app->run();
